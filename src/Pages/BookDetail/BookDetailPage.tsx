9bbb2bd6b35d995962decebf983d09a51039023f
funfordima <d.g.litvinov@gmail.com>
2021-03-25 22:40:09 +0200

    feat: add back button

--- src/Pages/BookDetail/BookDetailPage.tsx
+++ src/Pages/BookDetail/BookDetailPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import UserBar from '../../Components/UserBar/UserBarContainer';
 import Header from '../../Components/Header/HeaderContainer';
 import BookDescription from '../../Components/BookDescription/BookDescriptionContainer';
 import Footer from '../../Components/Footer/Footer';
+import { ReactComponent as ArrowSvg } from '../../public/arrow-left.svg';
+import { CATALOG_ROUTE } from '../../utils/constants';
 
 const Container = styled.div`
   width: 100%;
@@ -34,6 +37,58 @@ const Wrapper = styled.div`
   border-radius: .5rem;
 `;
 
+export const BackButton = styled(NavLink)`
+  padding: 16px 16px 16px 0;
+  display: inline-flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  font-size: 15px;
+  line-height: 24px;
+  color: #fff;
+  text-decoration: none;
+  transition-property: background-color, color;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-in-out;
+  cursor: pointer;
+
+  & span {
+    margin-left: 5px;
+    color: #573ba3;
+  }
+
+  & svg {
+    fill: #000;
+    vertical-align: bottom;
+    transform-origin: center;
+    transform: rotate(180deg);
+    border-radius: 50%;
+    background-color: #ccc;
+  }
+
+  &:hover {
+    color: rgba(255,255,255,0.8);
+  }
+
+  @media only screen and (max-width: 767px) and (min-width: 0) {
+    padding: 20px 16px;
+    color: rgba(0,0,0,0.48);
+
+    &:hover {
+      color: rgba(0,0,0,0.8);
+    }
+
+    & svg {
+      background-color: rgba(0,0,0,0.1);
+    }
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 // interface BookDetailPageProps {
 //   isAuth: boolean;
 // }
@@ -41,7 +96,13 @@ const Wrapper = styled.div`
 const BookDetailPage: React.FC = () => (
   <Container>
     <Wrapper>
-      <UserBar />
+      <Row>
+        <BackButton to={CATALOG_ROUTE}>
+          <ArrowSvg />
+          <span>Back to Catalog</span>
+        </BackButton>
+        <UserBar />
+      </Row>
       <Header />
       <BookDescription />
       <Footer />
